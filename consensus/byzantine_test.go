theaayushanand/tendermint

service#Start, service#Stop signatures were changed
See https://github.com/tendermint/tmlibs/issues/45

consensus/byzantine_test.go
@@ -67,7 +67,7 @@ func TestByzantine(t *testing.T) {
 
 		eventSwitch := events.NewEventSwitch()
 		eventSwitch.SetLogger(eventLogger.With("validator", i))
-		_, err := eventSwitch.Start()
+		err := eventSwitch.Start()
 		if err != nil {
 			t.Fatalf("Failed to start switch: %v", err)
 		}

consensus/replay_file.go
@@ -265,14 +265,14 @@ func newConsensusStateForReplay(config cfg.BaseConfig, csConfig *cfg.ConsensusCo
 	// Create proxyAppConn connection (consensus, mempool, query)
 	clientCreator := proxy.DefaultClientCreator(config.ProxyApp, config.ABCI, config.DBDir())
 	proxyApp := proxy.NewAppConns(clientCreator, NewHandshaker(state, blockStore))
-	_, err = proxyApp.Start()
+	err = proxyApp.Start()
 	if err != nil {
 		cmn.Exit(cmn.Fmt("Error starting proxy app conns: %v", err))
 	}
 
 	// Make event switch
 	eventSwitch := types.NewEventSwitch()
-	if _, err := eventSwitch.Start(); err != nil {
+	if err := eventSwitch.Start(); err != nil {
 		cmn.Exit(cmn.Fmt("Failed to start event switch: %v", err))
 	}
 

mnadamod/tendermint-go-copy

consensus/byzantine_test.go
@@ -58,7 +58,7 @@ func TestByzantine(t *testing.T) {
 
 		eventBus := types.NewEventBus()
 		eventBus.SetLogger(logger.With("module", "events", "validator", i))
-		_, err := eventBus.Start()
+		err := eventBus.Start()
 		require.NoError(t, err)
 		defer eventBus.Stop()
 

consensus/replay_file.go
@@ -292,13 +292,13 @@ func newConsensusStateForReplay(config cfg.BaseConfig, csConfig *cfg.ConsensusCo
 	// Create proxyAppConn connection (consensus, mempool, query)
 	clientCreator := proxy.DefaultClientCreator(config.ProxyApp, config.ABCI, config.DBDir())
 	proxyApp := proxy.NewAppConns(clientCreator, NewHandshaker(state, blockStore))
-	_, err = proxyApp.Start()
+	err = proxyApp.Start()
 	if err != nil {
 		cmn.Exit(cmn.Fmt("Error starting proxy app conns: %v", err))
 	}
 
 	eventBus := types.NewEventBus()
-	if _, err := eventBus.Start(); err != nil {
+	if err := eventBus.Start(); err != nil {
 		cmn.Exit(cmn.Fmt("Failed to start event bus: %v", err))
 	}
 